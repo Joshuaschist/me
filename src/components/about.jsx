7ed85051f4cbabb3f85017f884c94f2454b6d6b9
Chisom Joshua <chisomjoshua37@gmail.com>
2020-10-11 11:18:49 +0100

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am human developer who writes codes, solves problems and also very receptive to knowledge. I love exploring new technologies and often amazed by the progress we as a human beings have made so far in the recent years</p>
+                    <p>I have started reflecting my ideas and thoughts through my projects. You can checkout my <a href="https://joshuaschist.github.io/Landing-page/">Landing Page</a></p>
                     </div>
                 </div>
                 </div>
@@ -38,7 +38,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <p>I have experience building websites and applications using JavaScript,React,HTML,CSS</p>
                 </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <p>As coming from the JavaScript background, I have good grasp over fundamental concepts of DSA</p>
                 </div>
                 </div>
             </div>
@@ -59,8 +59,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Udemy(ZTM)</h3>
+                    <p>I am a partaker and member of ZeroToMastery(ZTM) Web-Developing organization on github, and also working with tools like Reactjs, and React-Native</p>
                 </div>
                 </div>
             </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,19 +21,9 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
-                      </div>
-                    </div>
-                  </article>
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-4">
-                        <i className="icon-pen2" />
-                      </div>
-                      <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2>Undergraduate at Unizik <span> 2017-till date</span> & ZTM Web Development Online(Udemy)<span>2020</span></h2>
+                        <p>Studying Psychology, but my major is in Artifical Psychology, which can be liken to as AI</p>
+                        <p>I am pursuing my programming career, with the help of Andrei tutoring in ZeroToMastery I have learnt courses both on Front-end and Backend(Fullstack) web development</p>
                       </div>
                     </div>
                   </article>
@@ -43,8 +33,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2>Primary and Higher Education <span>2001-2013</span></h2>
+                        <p>I completed my Primary and Higher Education at Hebron-Land and Glasshouse school</p> 
                       </div>
                     </div>
                   </article>
