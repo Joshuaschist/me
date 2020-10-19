0be18bc2a684857857a8a2b290a5bdb3d34c063c
Chisom Joshua <chisomjoshua37@gmail.com>
2020-10-19 17:06:54 +0100

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,9 +11,9 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am human developer who writes codes, solves problems and also very receptive to knowledge. I love exploring new technologies and often amazed by the progress we as a human beings have made so far in the recent years</p>
+                    <p>I am human developer who writes codes, solves problems and also very receptive to knowledge. I love exploring new technologies and often amazed by the progress we as human beings have made so far in the recent years</p>
                     <p>I have started reflecting my ideas and thoughts through my projects. You can checkout my <a href="https://joshuaschist.github.io/Landing-page/">Landing Page</a></p>
                     </div>
                 </div>
@@ -38,7 +38,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and applications using JavaScript,React,HTML,CSS</p>
+                    <p>I have experience building websites and applications using JavaScript,React,HTML &  CSS</p>
                 </div>
                 </div>
             </div>
