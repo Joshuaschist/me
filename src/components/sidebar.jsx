a6a597ca204def6ce73bc81722cccd2142ba5191
Joshua Ezem <chisomjoshua37@gmail.com>
2020-10-30 19:46:18 +0100

    Update Sidebar.js

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -26,7 +26,7 @@ export default class Sidebar extends Component {
             <nav id="colorlib-main-menu">
               <ul>
                 <li><a href="https://www.facebook.com/chis2zem" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/EzemChisom" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://twitter.com/EzemJosh" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                 <li><a href="https://www.instagram.com/Joshuaschist/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 <li><a href="https://www.linkedin.com/in/Chisom Ezem" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/Joshuaschist" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
